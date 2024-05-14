3f25e0b63f64f9d6c205aa883bdadbe4cc632e7e
Daniel Tan <danieltan380@gmail.com>
2024-05-13 22:43:07 -0700

    Render data from documents using shadcn card component.

--- poppins/src/components/FamilyProfile.jsx
+++ poppins/src/components/FamilyProfile.jsx
@@ -1,23 +1,42 @@
 import useFirestore from "../../hooks/useFirestore";
-import { useMemo } from "react";
+import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "./ui/card";
+
 
 const FamilyProfile = () => {
-  const { parentsDocs } = useFirestore();
+  const { parentsDocs, kidsDocs, familiesDocs } = useFirestore();
+  console.log(parentsDocs, kidsDocs, familiesDocs);
 
-  const data = [
-    {
-      firstName: 'John',
-      lastName: 'Smith'
-    },
-    {
-      firstName: 'Alice',
-      lastName: 'Smith'
-    }
-  ];
-
-
-
-  return (<div>hello</div>)
+  return (
+  <div>
+    {parentsDocs && parentsDocs.map(parent => (
+      <Card class="w-[400px] bg-gray-100 rounded-lg my-10 drop-shadow-lg" key={parent.id}>
+        <CardHeader>
+          <div>
+            <CardTitle>{parent.firstName} {parent.lastName}</CardTitle>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <p>{parent.email}</p>
+          <p>{parent.phone}</p>
+        </CardContent>
+      </Card>
+    ))}
+    {kidsDocs && kidsDocs.map(kid => (
+      <Card class="w-[400px] bg-gray-100 rounded-lg my-10 drop-shadow-lg" key={parent.id}>
+        <CardHeader>
+          <div>
+            <CardTitle>{kid.firstName} {kid.lastName}</CardTitle>
+            <CardDescription>{kid.birthdate}</CardDescription>
+          </div>
+        </CardHeader>
+        <CardContent>
+          <p>{kid.notes}</p>
+        </CardContent>
+      </Card>
+    ))}
+    
+  </div>
+)
 };
 
 export default FamilyProfile;
