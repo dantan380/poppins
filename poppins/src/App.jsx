38bc6f571e131aa11784457a4fe643311a5f6279
Daniel Tan <danieltan380@gmail.com>
2024-05-23 19:54:11 -0700

    Clean up App component to only show search bar and search results.

--- poppins/src/App.jsx
+++ poppins/src/App.jsx
@@ -1,22 +1,18 @@
+/* eslint-disable react/prop-types */
 import './App.css'
 import { useState } from 'react';
-import FamilyProfile from './components/FamilyProfile'
-import SearchBar from './components/SearchBar';
+import Layout from './layouts/Layout';
 import familyFetcher from './utils/dataFetcher/familyFetcher';
 
 function App() {
   const [families, setFamilies] = useState([]);
-  const [selectedFamilyId, setSelectedFamilyId] = useState(null);
   const [loading, setLoading] = useState(false);
-  const [members, setMembers] = useState([]);
   const [error, setError] = useState(null);
 
   const handleSearch = async (searchName) => {
     setLoading(true);
     setError(null);
     setFamilies([]);
-    setSelectedFamilyId(null);
-    setMembers([]);
 
     try {
       const fetchedFamilies = await familyFetcher.getFamilies({ familyName: searchName });
@@ -34,45 +30,9 @@ function App() {
     }
   };
 
-  const handleFamilySelect = async (familyId) => {
-    setLoading(true);
-    setError(null);
-    setSelectedFamilyId(familyId);
-    setMembers([]);
-
-    try {
-      const fetchedMembers = await familyFetcher.getFamilyMembers(familyId);
-      console.log('Fetched members:', fetchedMembers);
-      setMembers(fetchedMembers);
-    } catch (err) {
-      setError('Error fetching family members');
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  }
-
   return (
-    <div>
-      <SearchBar onSearch={handleSearch}/>
-      {error && <p>{error}</p>}
-      {loading && <div>Loading...</div>}
-      {!loading && families.length > 0 && (
-        <ul>
-          {families.map((family) => (
-            <li key={family.id}>
-              <button onClick={() => handleFamilySelect(family.id)}>
-                {family.familyName} ({family.id})
-              </button>
-            </li>
-          ))}
-        </ul>
-      )}
-      {!loading && selectedFamilyId && (
-        <FamilyProfile isLoading={loading} familyMembers={members}/>
-      )}
-    </div>
+    <Layout onSearch={handleSearch} families={families} loading={loading} error={error} ></Layout>
   );
 }
 
-export default App
+export default App;
