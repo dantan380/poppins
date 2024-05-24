ff0724430f45727ba5f92f0f7620b401d14c23f7
Daniel Tan <danieltan380@gmail.com>
2024-05-23 19:54:59 -0700

    Create router routes and insert RouterProvider to use client side routing.

--- poppins/src/main.jsx
+++ poppins/src/main.jsx
@@ -1,9 +1,22 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
+import FamilyProfilePage from './pages/FamilyProfilePage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: 'family/:familyId',
+    element: <FamilyProfilePage />
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
-    <App />
+   <RouterProvider router={router} />
   
 )
