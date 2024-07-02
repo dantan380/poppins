4762bd21fa7e215978e2bc6054840f513232e3c3
Daniel Tan <danieltan380@gmail.com>
2024-07-01 21:01:59 -0700

    Revise useEffect to use fetchCheckedInChildren with getCheckedIn method.

--- poppins/src/layouts/Layout.jsx
+++ poppins/src/layouts/Layout.jsx
@@ -2,31 +2,38 @@
 import { Outlet } from "react-router-dom";
 import SearchBar from "../components/SearchBar";
 import CheckInPage from "../pages/CheckInPage";
-import { useCheckIn } from "../context/CheckInContext";
 import { Card, CardTitle } from "../components/ui/card";
 import familyFetcher from "../utils/dataFetcher/familyFetcher";
 import { useState, useEffect } from "react";
 
 const Layout = ({ onSearch, families, loading, error }) => {
-  const { checkedIn } = useCheckIn();
+  const [recentDate, setRecentDate] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
   const [kidsCheckedIn, setKidsCheckedIn] = useState([]);
 
   useEffect(() => {
-    const fetchCheckedInKids = async () => {
-      try {
-        const kids = await familyFetcher.getCheckedIn(checkedIn);
-        setKidsCheckedIn(kids);
-      } catch (error) {
-        console.error(error);
-      }
-    };
+    fetchCheckedInChildren();
+  }, []);
 
-    fetchCheckedInKids();
-  }, [checkedIn])
+  const fetchCheckedInChildren = async () => {
+    setIsLoading(true);
+    
+    const result = await familyFetcher.getCheckedIn();
+
+    if (result.success) {
+      setKidsCheckedIn(result.checkedInChildrenDocs);
+      setRecentDate(result.date);
+    }
+    setIsLoading(false);
+  };
+
   return (
     <div>
       <SearchBar onSearch={onSearch} />
       <CheckInPage families={families} loading={loading} error={error}/>
+      {isLoading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      {recentDate && <h2>Report for {recentDate}</h2>}
       {kidsCheckedIn && kidsCheckedIn.map(kid => (
         <Card className='w-[400px] bg-slate-100 my-10 rounded-lg drop-shadow-xl' key={kid.id}>
           <CardTitle>{kid.firstName} {kid.lastName}</CardTitle>
