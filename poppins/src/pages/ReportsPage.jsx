4173b2eecd3b2240f15b68b8662139598b041d18
Daniel Tan <danieltan380@gmail.com>
2024-07-12 15:12:34 -0700

    Call getCheckedInWithDate method to test with ReportsPage.

--- poppins/src/pages/ReportsPage.jsx
+++ poppins/src/pages/ReportsPage.jsx
@@ -1,8 +1,32 @@
+import { useState, useEffect } from "react";
+import familyFetcher from "../utils/dataFetcher/familyFetcher";
+import { Card, CardTitle } from "../components/ui/card";
 
 const ReportsPage = () => {
+  const [kidsInReport, setKidsInReport] = useState([]);
+
+  const reportDate = "2024-07-05"
+
+  useEffect(() => {
+    fetchKidsInReport(reportDate);
+  }, []);
+
+  const fetchKidsInReport = async (inputDate) => {
+    const result = await familyFetcher.getCheckedInWithDate(inputDate);
+
+    if (result.success) {
+      setKidsInReport(result.checkedInChildrenDocs)
+    }
+  };
 
   return (
-    <div>Hello World</div>
+    <div>
+      {kidsInReport && kidsInReport.map(kid => (
+      <Card className='w-[400px] bg-slate-100 my-10 rounded-lg drop-shadow-xl' key={kid.id}>
+        <CardTitle>{kid.firstName} {kid.lastName}</CardTitle>
+      </Card>
+    ))}
+    </div>
   );
 };
 
