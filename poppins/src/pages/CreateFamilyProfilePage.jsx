06f13cb4296c18d7596e7751bfbe185cfa2d1683
Daniel Tan <danieltan380@gmail.com>
2024-07-16 22:26:44 -0700

    Create barebones CreateFamilyProfilePage component with form.

---
+++ poppins/src/pages/CreateFamilyProfilePage.jsx
@@ -0,0 +1,11 @@
+import FamilyProfileForm from "../components/FamilyProfileForm";
+
+const CreateFamilyProfilePage = () => {
+  return (
+    <div>
+      <FamilyProfileForm />
+    </div>
+  )
+}
+
+export default CreateFamilyProfilePage
